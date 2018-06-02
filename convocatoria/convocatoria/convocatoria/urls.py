b055092c25630b55b9980cd0342f8cc33b3754b1
Kevin <kevinsevinche@gmail.com>
2018-06-02 17:11:48 -0500

    Cambie el html para la página.

--- convocatoria/convocatoria/convocatoria/urls.py
+++ convocatoria/convocatoria/convocatoria/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('convocatoria.Apps.GestionConvocatoria.urls')),
 ]
