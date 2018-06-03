5dd8269b2477a5d96fbb103dc2299754427a1d17
Kevin <kevinsevinche@gmail.com>
2018-06-02 22:33:44 -0500

    Modified templates to display posts from database

--- convocatoria/convocatoria/convocatoria/Apps/GestionConvocatoria/views.py
+++ convocatoria/convocatoria/convocatoria/Apps/GestionConvocatoria/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Convocatoria
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'GestionConvocatoria/post_list.html', {})
+    convocatorias = Convocatoria.objects.all()
+    return render(request, 'GestionConvocatoria/post_list.html', {'convocatorias': convocatorias})
