143856e042ad467021071df466d9a3cd37dc6355
Kevin <kevinsevinche@gmail.com>
2018-06-03 03:09:07 -0500

    Added views to create/edit inside the side with forms

--- convocatoria/convocatoria/convocatoria/Apps/GestionConvocatoria/urls.py
+++ convocatoria/convocatoria/convocatoria/Apps/GestionConvocatoria/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^conv/(?P<pk>[0-9]+)/$', views.conv_detail, name='conv_detail'),
+    url(r'^conv/new/$', views.conv_new, name='conv_new'),
+    url(r'^conv/(?P<pk>[0-9]+)/edit/$', views.conv_edit, name='conv_edit'),
 ]
