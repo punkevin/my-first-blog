814d35fe7f306c2aedb636f87ecee5a945d189fe
Kevin <kevinsevinche@gmail.com>
2018-06-03 01:27:57 -0500

    Added views to create/edit blog post inside the site.

--- convocatoria/convocatoria/convocatoria/Apps/GestionConvocatoria/urls.py
+++ convocatoria/convocatoria/convocatoria/Apps/GestionConvocatoria/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^conv/(?P<pk>[0-9]+)/$', views.conv_detail, name='conv_detail'),
 ]
--- convocatoria/convocatoria/convocatoria/Apps/GestionConvocatoria/views.py
+++ convocatoria/convocatoria/convocatoria/Apps/GestionConvocatoria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Convocatoria
 
@@ -7,3 +7,7 @@ from .models import Convocatoria
 def post_list(request):
     convocatorias = Convocatoria.objects.all()
     return render(request, 'GestionConvocatoria/post_list.html', {'convocatorias': convocatorias})
+
+def conv_detail(request, pk):
+    convocatorias = get_object_or_404(Convocatoria, pk=pk)
+    return render(request, 'GestionConvocatoria/conv_detail.html', {'convocatorias': convocatorias})
